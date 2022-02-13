c425bd4ec545fe199c4fb2cfe1300ae78d25e275
Mo Yi <moyishi213@gmail.com>
2022-02-13 18:06:12 +0800

    Update leetcode_utils.py

--- codes/leetcode_utils.py
+++ codes/leetcode_utils.py
@@ -1023,3 +1023,59 @@ def matrix_pre_sum(data):
 def get_sub_matrix(pre, x1, y1, x2, y2):
     # 2边都能取到
     return pre[x2][y2] + pre[x1 - 1][y1 - 1] - pre[x2][y1 - 1] - pre[x1 - 1][y2]
+
+
+
+class KM:
+    INF = int(1e9)
+
+    def __init__(self, graph):
+        ma = max(len(graph), len(graph[0]))
+        self.graph = graph
+        self.vis_x = set()
+        self.vis_y = set()
+        self.match = [-1] * ma
+        self.lx = [max(row) for row in graph]
+        self.ly = [0] * ma
+        self.slack = []
+
+        self.nx, self.ny = len(graph), len(graph[0])
+
+    def find_path(self, x):
+        self.vis_x.add(x)
+        for y in range(self.ny):
+            if y in self.vis_y:
+                continue
+            tmp_delta = self.lx[x] + self.ly[y] - self.graph[x][y]
+            if tmp_delta == 0:
+                self.vis_y.add(y)
+                if self.match[y] == -1 or self.find_path(self.match[y]):
+                    self.match[y] = x
+                    return True
+            elif self.slack[y] > tmp_delta:
+                self.slack[y] = tmp_delta
+        return False
+
+    def solve(self):
+        for x in range(self.nx):
+            self.slack = [KM.INF] * self.ny
+            while True:
+                self.vis_x.clear()
+                self.vis_y.clear()
+                if self.find_path(x):
+                    break
+                else:
+                    delta = KM.INF
+                    for j in range(self.ny):
+                        if j not in self.vis_y and delta > self.slack[j]:
+                            delta = self.slack[j]
+                    for i in range(self.nx):
+                        if i in self.vis_x:
+                            self.lx[i] -= delta
+                    for j in range(self.ny):
+                        if j in self.vis_y:
+                            self.ly[j] += delta
+                        else:
+                            self.slack[j] -= delta
+
+        return self.match
