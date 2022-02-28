1fe7a3962fa6bb24c0313a1a9b4a602edfba999c
MoYi <moyi@galaxyeye-tech.com>
2022-02-28 15:26:38 +0800

    update

--- codes/shopee_code_league/2020#1#2.py
+++ codes/shopee_code_league/2020#1#2.py
@@ -34,22 +34,61 @@ def solve1():
             print(ans)
 
 
+class KarpRabin:
+    big_primes = [16777619]
+    maxInt = 1 << 32
+    to_int = ord
+
+    def __init__(self):
+        from collections import deque
+        self.hash = [0 for _ in KarpRabin.big_primes]
+        self.string = deque()
+
+    def append(self, char):
+        for i in range(len(KarpRabin.big_primes)):
+            self.hash[i] = ((self.hash[i] * KarpRabin.big_primes[i]) + KarpRabin.to_int(char)) % KarpRabin.maxInt
+        self.string.append(char)
+        return self
+
+    def extend(self, string):
+        for char in string:
+            self.append(char)
+        return self
+
+    def popleft(self):
+        char = self.string.popleft()
+        for i in range(len(KarpRabin.big_primes)):
+            self.hash[i] -= pow(KarpRabin.big_primes[i], len(self.string), KarpRabin.maxInt) * KarpRabin.to_int(char)
+            self.hash[i] %= KarpRabin.maxInt
+        return self
+
+    def hash_value(self):
+        return tuple(self.hash)
+
+    def to_string(self):
+        return ''.join(list(self.string))
+
+
 def solve2():
-    # 下面写法时间复杂度是O(121 * N), 空间复杂度O(121 * N).
-    # 用字符串哈希时间复杂度可以到O(11 * N), 空间复杂度有点难算, 应该比O(121 * N)小.
-    from collections import defaultdict
+    # 时间复杂度O(11 * N), 空间复杂度有点难算, 应该比O(121 * N)小.
+    from collections import Counter
     for _ in range(cin_int()):
         n, q = cin_int_ls()
-        invert_index = defaultdict(set)
+        invert_index = Counter()
         for i in range(n):
             item = input().split()
-            # 这里用字符串哈希可以到O(11 * n)
+            tmp = set()
             for l in range(0, len(item)):
-                for r in range(l + 1, len(item) + 1):
-                    invert_index[tuple(item[l:r])].add(i)
+                k = KarpRabin()
+                for r in range(l, len(item)):
+                    k.extend(item[r])
+                    tmp.add(k.hash_value())
+                    k.append(' ')
+            for h in tmp:
+                invert_index[h] += 1
         for _ in range(q):
-            ans = invert_index[tuple(input().split())]
-            print(len(ans))
+            ans = invert_index[KarpRabin().extend(input()).hash_value()]
+            print(ans)
 
 
 # solve1()
