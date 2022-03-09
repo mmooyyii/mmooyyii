7e3fa7d89a733aa2a1cd922ecb7895d9bb1b97db
yimo <moyishi213@gmail.com>
2022-03-09 23:33:27 +0800

    update

--- codes/shopee_code_league/2021/c.py
+++ codes/shopee_code_league/2021/c.py
@@ -13,18 +13,39 @@ def cin_int():
     return int(input())
 
 
-from collections import defaultdict
+def floyd(n, g: dict):
+    ret = [[float('inf') for _ in range(n)] for _ in range(n)]
+    for node, route in g.items():
+        ret[node][node] = 0
+        for v, w in route.items():
+            ret[node][v] = w
+            ret[v][node] = w
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if ret[i][j] > ret[i][k] + ret[k][j]:
+                    ret[i][j] = ret[i][k] + ret[k][j]
+    return ret
+
+
+from collections import defaultdict, Counter
 
 n_city, n_warehouse, n_edges = cin_int_ls()
-graph = defaultdict(set)
+graph = defaultdict(dict)
 for _ in range(n_edges):
     a, b = cin_int_ls()
-    graph[a].add(b)
-    graph[b].add(a)
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+a = floyd(n_city + 1, graph)
+for i in a:
+    print(i)
 
 for _ in range(n_warehouse):
     dto, fee, city = cin_int_ls()
 
-custom = []
+custom = Counter()
 for _ in range(cin_int()):
     n, city = cin_int_ls()
+    custom[city] += n
+print(custom)
--- codes/shopee_code_league/2021/d.py
+++ codes/shopee_code_league/2021/d.py
@@ -11,3 +11,32 @@ def cin_int_ls():
 
 def cin_int():
     return int(input())
+
+
+n, k = cin_int_ls()
+ls = cin_int_ls()
+
+
+def check(threshold):
+    a, b = 1, 1
+    need = 0
+    for i in ls:
+        if (a + i) * (b + 1) > threshold:
+            a = i
+            b = 1
+            need += 1
+        else:
+            a += i
+            b += 1
+    return need <= k
+
+# 2分答案， 感觉挺对的
+left, right = 0, 1 << 64
+while left < right:
+    mid = (right + left) // 2
+    if check(mid):
+        right = mid
+    else:
+        left = mid + 1
+
+print(left * k)
